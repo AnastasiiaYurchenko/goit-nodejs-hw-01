e66f31c76771ddcc28f1d9acf4adf77dd3393568
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-06-22 00:20:10 +0300

    add functions

--- contacts.js
+++ contacts.js
@@ -1,21 +1,65 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+// console.log(__dirname);
+// console.log(contactsPath);
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код. Повертає масив контактів.
-}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+};
 
-function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
 
-function addContact(name, email, phone) {
+const addContact = async (name, email, phone) => {
   // ...твій код. Повертає об'єкт доданого контакту.
-}
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+// const add = async (data) => {
+//   //беремо масив з книгами
+//   const books = await getAll();
+//   // створюємо нову книгу: розпилюємо дані і додаємо id
+//   const newBook = {
+//     id: nanoid(),
+//     ...data,
+//   };
+//   // додаємо в масив нову книгу
+//   books.push(newBook);
+//   // перезаписуємо книги
+//   await fs.writeFile(booksPath, JSON.stringify(books, null, 2)); // щоб нормально записалось, а не в одну строку
+//   return newBook;
+// };
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello World!!!!!!");
+const contacts = require("./contacts");
+// console.log(contacts);
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      return console.log(removeContact);
+    case "add":
+      const addContact = await contacts.addContact(name, email, phone);
+      return console.log(addContact);
+  }
+};
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "qdggE76Jtbfd9eWJHrssH" });
+// invokeAction({ action: "remove", id: "qdggE76Jtbfd9eWJHrssH" });
+invokeAction({
+  action: "add",
+  name: "aaaaaaaa",
+  email: "dfdfdg@tyy.jhg",
+  phone: "657657676",
+});
